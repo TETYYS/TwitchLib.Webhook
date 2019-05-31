4400638253aea1042e5d7d5f1bc4de2577649dd3
tetyys <tetyys@tetyys.com>
2019-06-01 02:26:07 +0300

    Change model

--- src/TwitchLib.Webhook/Models/User.cs
+++ src/TwitchLib.Webhook/Models/User.cs
@@ -6,14 +6,6 @@ namespace TwitchLib.Webhook.Models
 	public class User
 	{
 		[JsonProperty("data")]
-		public UserData Data { get; set; }
-		[JsonProperty("id")]
-		public string Id { get; set; }
-		[JsonProperty("timestamp")]
-		public DateTime Timestamp { get; set; }
-		[JsonProperty("topic")]
-		public string Topic { get; set; }
-		[JsonProperty("type")]
-		public string Type { get; set; }
+		public UserData[] Data { get; set; }
 	}
 }
